e6431052d71d58edd18728eb2fa89ab59b30d657
christian-bromann <mail@christian-bromann.com>
2017-09-28 11:59:24 +0200

    add stuff

---
+++ test/test.e2e.js
@@ -0,0 +1,21 @@
+const assert = require('assert')
+
+describe('site test', () => {
+    before(() => browser.url('http://localhost:8080'))
+    
+    it('should have opened correct page', () => {
+        assert.deepEqual(browser.getTitle(), 'Jumbotron Template for Bootstrap');
+    })
+    
+    it('should open modal', () => {
+        $('#modal').click()
+        $('#exampleModalLong').waitForVisible()
+        assert($('#exampleModalLong').isVisible())
+        assert.deepEqual($('#exampleModalLong .modal-body').getText(), 'Hello There')
+    })
+    
+    it('should close modal', () => {
+        $('#closeit').click()
+        $('#closeit').waitForVisible(3000, true)
+    })
+})
