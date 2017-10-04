9a854b31084227116120456c36919b29edf4ff25
christian-bromann <mail@christian-bromann.com>
2017-10-04 11:55:37 +0200

    submit login form

--- test/test.e2e.js
+++ test/test.e2e.js
@@ -27,6 +27,7 @@ describe('site test', () => {
         it('should try to login', () => {
             $('#username').setValue('admin')
             $('#password').setValue('mysupersecretpassword')
+            $('#login').click()
             browser.waitUntil(() => browser.getTitle() === 'Admin Area', 5000, 'Could not log in')
         })
     })
