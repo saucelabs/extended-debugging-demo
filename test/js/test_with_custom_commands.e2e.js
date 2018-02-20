f18dccd3e45b502cf02844088e6cd74196358062
christian-bromann <mail@christian-bromann.com>
2018-02-20 14:58:34 +0100

    add test for updating todos using custom commands

--- test/js/test_with_custom_commands.e2e.js
+++ test/js/test_with_custom_commands.e2e.js
@@ -7,14 +7,17 @@ describe('test without custom commands', () => {
                 url: 'http://localhost:8080/api/todos',
                 response: {
                     body: [{
+                        id: 0,
                         title: 'My custom Todo #1',
                         order: 1,
                         completed: false
                     }, {
+                        id: 1,
                         title: 'My custom Todo #2',
                         order: 2,
                         completed: true
                     }, {
+                        id: 2,
                         title: 'My custom Todo #3',
                         order: 3,
                         completed: false
@@ -31,5 +34,27 @@ describe('test without custom commands', () => {
         })
     })
 
-    after(() => browser.pause(3000))
+    describe('update todo', () => {
+        before(() => {
+            browser.execute('sauce:intercept', {
+                url: 'http://localhost:8080/api/todos/2',
+                response: {
+                    body: {
+                        id: 2,
+                        title: 'My custom Todo #3',
+                        order: 3,
+                        completed: true
+                    }
+                }
+            })
+        })
+
+        it('should update todo', () => {
+            const thirdTodo = $$('#todo-list li')[2]
+            thirdTodo.$('.toggle').click()
+
+            const classNames = thirdTodo.getAttribute('class')
+            assert.ok(classNames.includes('completed'))
+        })
+    })
 })
