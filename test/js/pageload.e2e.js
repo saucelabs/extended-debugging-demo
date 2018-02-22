8bf2ca5e3f43056e2aeb078728b8805e70ce0cfa
christian-bromann <mail@christian-bromann.com>
2018-02-22 21:00:01 +0100

    update tests based on changes on crmuxdriver

--- test/js/pageload.e2e.js
+++ test/js/pageload.e2e.js
@@ -2,12 +2,12 @@ import assert from 'assert'
 import getLoadTime from './helpers/getLoadTime'
 
 describe('my demo app', () => {
-    describe.skip('loads app with third party script', () => {
+    describe('loads app with third party script', () => {
         it('should load fast', () => {
             browser.url('/')
 
-            const { value: { performanceMetrics } } = browser.log('sauce:metrics')
-            const pageLoadTime = performanceMetrics.DomContentLoaded - performanceMetrics.NavigationStart
+            const { value } = browser.log('sauce:metrics')
+            const pageLoadTime = value.domContentLoaded - value.navigationStart
             assert.ok(pageLoadTime <= 5, `Expected page load time to be lower than 5s but was ${pageLoadTime}s`)
         })
 
